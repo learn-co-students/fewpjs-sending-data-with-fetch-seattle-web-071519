86ebd58f2ead260abe6174d98ebd41bcf5df8b2c
Nick Dunlap <nwdunlap17@gmail.com>
2019-08-29 10:05:26 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+function submitData(name,email){
+    let configObject = {
+        method: 'Post',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email
+
+        })
+    }
+    return fetch('http://localhost:3000/users',configObject)
+            .then(response => response.json())
+            .then(json => appendToDOM(json.id))
+            .catch(response => appendToDOM(response))
+}
+
+function appendToDOM(string){
+    console.log(string)
+    let paragraph =  document.createElement("p")
+    paragraph.textContent = string
+   document.body.appendChild(paragraph)
+}
