2c58d9830dd039711d5dcff954c54b05409a91ac
marshallhelenm <marshallhelenm@gmail.com>
2019-08-29 11:29:07 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+document.addEventListener("DOMContentLoaded", main)
+
+function main(event){
+    submitData("helen", "email@email.com");
+
+
+}
+
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email
+        })
+    }).then(response => response.json())
+        .then(json => appendToDOM(json.id))
+        .catch(error =>appendToDOM(error.message) )
+}
+
+function appendToDOM(thing){
+    let pTag = document.createElement('p')
+    pTag.textContent = thing
+    document.body.appendChild(pTag)
+}
+
+// function idToScreen(json){
+    
+//     let pTag = document.createElement('p')
+//     pTag.textContent = json.id
+//     document.body.appendChild(pTag)
+// }
