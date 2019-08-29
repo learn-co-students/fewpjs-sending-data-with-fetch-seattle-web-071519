43eb2eb50fb888e19805f9e4bb4273ed47a7f7a9
ayerest <irisiseve@gmail.com>
2019-08-29 08:56:33 -0700

    finished sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Add your code here
+// let destinationURL = "http://localhost:3000/dogs"
+
+// let configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         name,
+//         email
+//     })
+// };
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(response => response.json())
+    .then(function(json) {
+        document.body.innerHTML = json["id"]
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    })
+}
+
+// function handleStuff(object) {
+//     document.body.innerHTML = object["id"]
+// }
+
+// fetch(destinationURL, configurationObject)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object) {
+//         console.log(object);
+//     })
+//     .catch(function (error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+//     });
